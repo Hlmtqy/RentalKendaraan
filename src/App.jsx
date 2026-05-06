6fd479b57e5dbe928f8794b1928b9e3b158647fd
Hlmtqy <taqyhilmi07@gmail.com>
2026-05-06 07:30:34 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex justify-center">
+      <div className="w-full max-w-100 bg-gray-50 min-h-screen relative shadow-2xl border-red-500 border"></div>
+    </div>
+  )
+}
+
+export default App
